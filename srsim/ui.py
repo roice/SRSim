01c018778dd15b8e1a4eea2d229a978a8dcba15d
roice <xingqing_lhtv@qq.com>
2015-09-07 20:39:19 +0800

    working on ui

--- srsim/ui.py
+++ srsim/ui.py
@@ -1,5 +1,35 @@
+#!/usr/bin/python
 # coding=utf-8
+#
+# sniffer robots simulator user interface
+#
+# Author: Roice Luo <oroice@foxmail.com>
+# copyright (c) 2015 Roice Luo <https://github.com/roice>
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 2.1 of the License, or (at your
+# option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
-import algorithm
+'''srsim ui module
 
-print algorithm.name
+Documentation and tests are included in ...
+'''
+
+# Major scientific library imports
+import numpy as np
+
+#############################################################################
+# Module-level variables
+
+# The grid of points on which we want to evaluate the field
+X, Y, Z = np.mgrid[-0.15]
